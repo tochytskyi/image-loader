23d9e6c10109bde6e3f8aa8ca0034cef08f8091f
Volodymyr Tochytskyi <v.tochytskyi@gmail.com>
2016-02-13 17:06:20 +0200

    create 'uploads' folder if it doesn't exist

--- server/save-image.php
+++ server/save-image.php
@@ -4,10 +4,19 @@
 $image = $_FILES['image'];
 $rotation = $_POST['rotation'];
 
+if (empty($image)) {
+	echo json_encode(array('error' => 'No images'));
+	die('No images');
+}
+
 $uploaddir = 'uploads/';
 $absolutedir = 'server/';
 $path_parts = pathinfo($image['name']);
 $uploadfile = $uploaddir . $path_parts['filename'] . '_' . uniqid() . '.' .$path_parts['extension'];
+
+if (!file_exists($uploaddir)) {
+    mkdir($uploaddir, 0777, true);
+}
 
 
 $error = false;
